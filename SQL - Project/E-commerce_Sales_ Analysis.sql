f1dabdfe8d7eb3a2f96fc12502098ff93472a0fd
mallelasrilatha97-ux <mallela.srilatha97@gmail.com>
2026-03-12 08:17:19 +0530

    Create E-commerce_Sales_ Analysis.sql

---
+++ SQL - Project/E-commerce_Sales_ Analysis.sql
@@ -0,0 +1,21 @@
+# Project Title
+E-commerce Sales Analysis Using SQL
+
+# Project Description
+Analyzed e-commerce transaction data to identify sales trends, customer purchasing behavior, top-performing products,
+and regional sales performance using SQL queries. The analysis helps businesses understand revenue patterns and 
+optimize product strategy to improve overall sales performance.
+
+# Dataset contains
+- Order ID
+- Customer ID
+- Product Name
+- Category
+- Sales Amount
+- Order Date
+- Region
+- Quantity
+
+# Project Focus
+The project focuses on identifying top-selling products, revenue trends, regional sales performance,
+  and average order value to support data-driven business decisions.
